4ecdf6e8afd245f3a25ee668b1939717a13c11ba
ozaman07 <semi_clever@hotmail.com>
2017-10-05 22:36:38 +0700

    c

--- addplayer.php
+++ addplayer.php
@@ -244,7 +244,7 @@
                                                 <label class="control-label " id="label-addplayer" >Release Date</label>
                                                 <input type="date" class="form-control" size="100" ng-model="release_date">
                                             </div>
-                                            <div class="form-group form-inline">
+                                            <div class="form-group">
                                                 <label class="control-label " id="label-addplayer" >Release Date</label>
                                                 <input type="date" class="form-control" size="100" ng-model="release_date">
                                             </div>
